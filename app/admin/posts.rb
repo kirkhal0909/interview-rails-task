83b3045620f964c42da9887ef0905ea834815806
kirkhal0909 <kirkhal0909@gmail.com>
2022-04-28 02:22:13 +0300

    restore version post.message

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register Post do
   permit_params :message
+
+  member_action :history do
+    @post = Post.find(params[:id])
+    # @versions = @post.versions # <-- Sadly, versions aren't available in this scope, so use:
+    @versions = PaperTrail::Version.where(item_type: 'Post', item_id: @post.id)
+    render "layouts/history"
+  end
+
+  index do
+    column :id
+    column :message
+    actions defaults: true do |post|
+      link_to 'History', history_admin_post_path(post)
+    end
+  end
 end
